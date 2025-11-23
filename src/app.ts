36888e6bb620a0712442869846fd31890ce4fd17
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-11-22 20:36:59 -0500

    Initialize Express application with middleware and error handling

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require( "cors");
+const morgan = require( "morgan");
+const routes = require( "./routes");
+const { errorHandler } = require( "./middlewares/errorMiddleware");
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", routes);
+
+app.use(errorHandler);
+
+module.exports = app;
