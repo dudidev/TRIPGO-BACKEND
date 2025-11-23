a2e1c372fdd439c671fb55384dbcab5a5d4a2c6e
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-11-22 20:32:25 -0500

    Add and Rename planeadorRoutes with CRUD operations for Planeador

---
+++ src/routes/servicioEstablecimientosRoutes.ts
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const {ServicioEstablecimientoController} = require("../controllers/servicioEstablecimientoController");
+
+const router = Router();
+router.post("/", ServicioEstablecimientoController.crear);
+
+module.exports = router;
