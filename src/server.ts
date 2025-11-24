0487da6a2030a62bfd2145ab887e7a5c806998ea
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-11-24 10:39:50 -0500

    Refactor port 4000

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`TripGO backend corriendo en http://localhost:${PORT}/api`);
