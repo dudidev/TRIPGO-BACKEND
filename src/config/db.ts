7047d0dcb5b0a3cbcc01174392283c8e98d7d75a
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-11-24 09:54:18 -0500

    Add db port

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,6 +8,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASS || "",
     database: process.env.DB_NAME || "tripgo",
+    port: Number(process.env.DB_PORT) || 3306,
     waitForConnections: true,
     connectionLimit: parseInt(process.env.DB_CONN_LIMIT || "10", 10),
     queueLimit: 0
