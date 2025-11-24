421c6af621d25b30e63c982253610e43f5e36655
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-11-24 10:29:45 -0500

    Delete for rename file

--- src/services/establecimientoServicio.ts
+++
@@ -1,7 +0,0 @@
-const { EstablecimientoRepo } = require ("../repositories/establecimientoRepo");
-class EstablecimientoService {
-    static async listar() { return EstablecimientoRepo.listar(); }
-    static async crear(e: any) { return EstablecimientoRepo.crear(e); }
-}
-
-module.exports = { EstablecimientoService };
