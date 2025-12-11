2f3b17e43aaec3ee804d71588411ad154eae3619
Duvier SENA <duvierdiegodavid.0715@gmail.com>
2025-12-11 11:44:19 -0500

    Refactor db.ts to pool database and get connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,17 +1,24 @@
 const mysql = require("mysql2/promise");
+const fs = require('fs');
+const path = require('path');
 const dotenv = require("dotenv");
 
 dotenv.config();
 
+const sslCert = path.resolve(__dirname, 'BaltimoreCyberTrustRoot.crt.pem');
+
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "",
-    database: process.env.DB_NAME || "tripgo",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     port: Number(process.env.DB_PORT) || 3306,
     waitForConnections: true,
     connectionLimit: parseInt(process.env.DB_CONN_LIMIT || "10", 10),
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+    ca: fs.readFileSync(sslCert)
+  }
 });
 
 module.exports = { pool };
