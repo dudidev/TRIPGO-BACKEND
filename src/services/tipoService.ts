4851c270e3f6dbf5b4e77b9809c6da91d47be1f7
dudidev <duvierdiegodavid.0715@gmail.com>
2025-12-13 15:10:15 -0500

    Refactor listar method in TipoService for improved readability

--- src/services/tipoService.ts
+++ src/services/tipoService.ts
@@ -1,10 +1,8 @@
-// src/services/Tipo.service.ts
 const { TipoRepo } = require("../repositories/tipoRepo");
 
 class TipoService {
-    static async listar() {
-        return await TipoRepo.listar();
-    }
+    static async listar() { return await TipoRepo.listar(); }
+    static async crear(t: any) { return await TipoRepo.crear(t); }
 }
 
 module.exports = { TipoService };
