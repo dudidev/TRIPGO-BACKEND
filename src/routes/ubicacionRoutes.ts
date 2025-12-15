4a90227b30315cecd9d5db013269583403a2caed
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-12-14 19:33:55 -0500

    Remove Ubicacion module and related files for refactoring

--- src/routes/ubicacionRoutes.ts
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { UbicacionController } = require("../controllers/ubicacionController");
-
-const router = Router();
-router.get("/", UbicacionController.listar);
-router.post("/", UbicacionController.crear);
-
-module.exports = router;
