1cfbe9d540e1b177429456fec80c72fd3c4e2f70
Duvier David <duvierdiegodavid.0715@gmail.com>
2026-02-15 22:11:46 -0500

    Refactor: Simplify console log messages

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ dotenv.config();
 
 const app = require("./app");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`TripGO backend corriendo en ${PORT}`);
+    console.log(`TripGO backend corriendo en http://localhost:${PORT}/api`);
 });
