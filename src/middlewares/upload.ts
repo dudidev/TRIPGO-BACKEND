cfa172c046b2af9277468fbe7a99b77422864190
Shaicausil <causil845@gmail.com>
2026-03-04 17:35:59 -0500

    solo formato imagen

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -2,9 +2,26 @@ const multer = require("multer");
 
 const upload = multer({
   storage: multer.memoryStorage(),
+
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB
+    fileSize: 5 * 1024 * 1024, 
   },
+
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = [
+      "image/jpeg",
+      "image/jpg",
+      "image/png",
+      "image/webp",
+      "image/gif"
+    ];
+
+    if (allowedTypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Solo se permiten archivos de tipo imagen"), false);
+    }
+  }
 });
 
 module.exports = upload;
