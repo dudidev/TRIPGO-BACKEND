c7fe5db428c183942e9d2a842ee73a488ba9e6e6
Duvier David <duvierdiegodavid.0715@gmail.com>
2026-03-10 21:02:52 -0500

    Delete: renombra comentario por resena

--- src/models/comentarioModel.ts
+++
@@ -1,8 +0,0 @@
-export interface Comentario {
-    id_comentarios?: number;
-    comentario: string;
-    id_usuario: number;
-    id_ubicacion: number;
-    id_establecimiento?: number | null;
-    fecha_publicacion?: Date;
-}
