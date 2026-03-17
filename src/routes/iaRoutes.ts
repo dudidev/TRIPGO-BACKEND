89f615ec90e709e20c29cbe7abec5fca2011729d
Duvier David <duvierdiegodavid.0715@gmail.com>
2026-03-17 10:42:50 -0500

    Refactor: en imports y metodos, quitando codigo basura

--- src/routes/iaRoutes.ts
+++ src/routes/iaRoutes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import { chatbotIA, recomendacionesIA } from "../controllers/iaController.js";
+import { chatbotIA} from "../controllers/iaController.js";
 
 const router = Router();
 
 router.post("/chatbot", chatbotIA);
-
-router.get("/recomendaciones", recomendacionesIA);
 
 export default router;
