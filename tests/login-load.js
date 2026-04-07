4f97ad9caa0832654fa072a4177c1e6e897b9925
Duvier David <duvierdiegodavid.0715@gmail.com>
2026-04-07 11:18:41 -0500

    feat: test para carga de usuarios

---
+++ tests/login-load.js
@@ -0,0 +1,38 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '20s', target: 5 },
+        { duration: '20s', target: 10 },
+        { duration: '20s', target: 20 },
+        { duration: '20s', target: 30 },
+        { duration: '20s', target: 50 },
+        { duration: '20s', target: 0 },
+    ],
+    thresholds: {
+        http_req_duration: ['p(95)<1000'],
+        http_req_failed: ['rate<0.05'],
+    },
+};
+
+export default function () {
+    const res = http.post(
+        'http://localhost:8080/auth/login',
+        JSON.stringify({
+            correo_usuario: 'usuario@test.com',
+            password_u: '123456',
+        }),
+        {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+    );
+
+    check(res, {
+        'status 200': (r) => r.status === 200,
+    });
+
+    sleep(1);
+}
