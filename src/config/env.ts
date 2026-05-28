c6ba690b026e2db7779bd604a54f9eb1bf001f65
Duvier David <duvierdiegodavid.0715@gmail.com>
2026-05-28 17:47:42 -0500

    arranca .env siempre

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
 
-if (process.env.NODE_ENV !== "production") {
-    const envFile = `.env.${process.env.NODE_ENV || "development"}`;
-    dotenv.config({ path: envFile });
-    console.log("ENV cargado:", envFile);
-}
+const envFile = process.env.NODE_ENV === "production"
+        ? ".env.production"
+        : ".env.development";
+
+dotenv.config({ path: envFile });
+
+console.log("ENV cargado:", envFile);
